54a3e1cf934221c8acf27b685a96078dd67f9ba1
xdprolul <fireeagle2014@gmail.com>
2026-01-27 19:01:20 +0530

    pokedex start 'REPL'

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, World!")
+	scanner:=bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Printf("Pokedex > ")
+		input:=""
+		if scanner.Scan(){
+			input=scanner.Text()
+		}
+		cleanedWords:=cleanInput(input)
+		fmt.Printf("Your command was: %v\n",cleanedWords[0])
+	}
 }
